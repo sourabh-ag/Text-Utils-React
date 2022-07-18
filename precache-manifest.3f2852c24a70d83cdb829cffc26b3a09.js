e3b7cc9cabe3327257eb00e0c94a92e044f6ec95
Sourabh Agarwal <100523765+sourabh-ag@users.noreply.github.com>
2022-07-18 16:36:57 +0530

    Add files via upload

---
+++ precache-manifest.3f2852c24a70d83cdb829cffc26b3a09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "17052c052f83298d5fcf",
+    "url": "/Text-Utils-React/static/css/main.9d8556d6.chunk.css"
+  },
+  {
+    "revision": "17052c052f83298d5fcf",
+    "url": "/Text-Utils-React/static/js/main.17052c05.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/Text-Utils-React/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "fc6bf2a49d77b21506bb",
+    "url": "/Text-Utils-React/static/js/2.fc6bf2a4.chunk.js"
+  },
+  {
+    "revision": "6fe8df2cc1e4e22b91a1",
+    "url": "/Text-Utils-React/static/js/runtime~main.6fe8df2c.js"
+  },
+  {
+    "revision": "5269a388ab2a4846489f40fee1257115",
+    "url": "/Text-Utils-React/index.html"
+  }
+];
